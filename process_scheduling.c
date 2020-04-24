42ee660adb8348c855e53525daeb6caa7c8f4fda
gameow1124 <steven861124@gmail.com>
2020-04-24 15:49:33 +0800

    catch policy

--- process_scheduling.c
+++ process_scheduling.c
@@ -1,6 +1,7 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 typedef struct child{
 	int id;
 	char name[100];
@@ -15,16 +16,26 @@ typedef struct child{
 
 int main(int argc, char* argv[])
 {
-	char policy[10];
+	char policy_name[10];
 	int child_num;
+	int policy = 0;
 	child child_q[100];
-	scanf("%s",policy);
+	scanf("%s",policy_name);
 	scanf("%d",&child_num);
 	for(int i = 0;i < child_num;i++){
 		child_q[i].id = i;
 		scanf("%s%d%d",child_q[i].name, &child_q[i].rd_t, &child_q[i].ex_t);
 	}
-	
+	if(strcmp(policy_name, "FIFO") == 0)
+		policy = 0;
+	else if(strcmp(policy_name, "RR") == 0)
+		policy = 1;
+	else if(strcmp(policy_name, "SJF") == 0)
+		policy = 2;
+	else
+		policy = 3;
+	printf("%d",policy);
+		
 
 
 	return 0;
