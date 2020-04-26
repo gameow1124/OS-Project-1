430c5bc943c58adc33b3b06250bfeed12127fc38
gameow1124 <steven861124@gmail.com>
2020-04-26 16:10:02 +0800

    RR

--- process_scheduling.c
+++ process_scheduling.c
@@ -12,6 +12,10 @@ typedef struct child{
 	int rd_t;
 	int ex_t;
 	int left_t;
+	long long start_sec;
+	long long start_nsec;
+	long long end_sec;
+	long long end_nsec;
 	pid_t pid;
 }child;
 
@@ -73,11 +77,11 @@ void fork_child(int who)
 		int exec_time = child_q[who].ex_t;
 		for(int j = 0;j<exec_time;j++)
 			unittime();
-		fprintf(stderr,"%s %d\n", child_q[who].name, getpid());
+		fprintf(stderr,"finish %s %d\n", child_q[who].name, getpid());
 		exit(0);
 	}
-	setIDLE(pid);
 	child_q[who].pid = pid;
+	setIDLE(pid);
 }
 
 int main(int argc, char* argv[])
@@ -87,6 +91,7 @@ int main(int argc, char* argv[])
 	int policy = 0;
 	scanf("%s",policy_name);
 	scanf("%d",&child_num);
+	setACTIVE(getpid());
 	for(int i = 0;i < child_num;i++){
 		scanf("%s%d%d",child_q[i].name, &child_q[i].rd_t, &child_q[i].ex_t);
 		child_q[i].left_t = -1;
@@ -104,18 +109,10 @@ int main(int argc, char* argv[])
 	int finish_child = 0;
 	int counter = 0;
 	int now_run = -1;
+	int t_q = 0;
 	qsort(child_q, child_num, sizeof(child),compare);
 	while(finish_child != child_num)
 	{
-		if(now_run != -1 && child_q[now_run].left_t == 0)
-		{
-			int status;
-			waitpid(child_q[now_run].pid,&status,0);
-			now_run = -1;
-			finish_child++;
-		}
-
-
 		for(int i = 0;i<child_num;i++)
 		{
 			if(child_q[i].rd_t == counter)
@@ -123,9 +120,51 @@ int main(int argc, char* argv[])
 				fork_child(i);
 			}
 		}
+
+		if(now_run != -1 && child_q[now_run].left_t == 0)
+		{
+			int status;
+			waitpid(child_q[now_run].pid,&status,0);
+			syscall(333, &child_q[now_run].end_sec,&child_q[now_run].end_nsec);
+			char buff[1000];
+			sprintf(buff,"[Project1]%d %lld.%09lld %lld.%09lld",child_q[now_run].pid,child_q[now_run].start_sec,child_q[now_run].start_nsec,child_q[now_run].end_sec,child_q[now_run].end_nsec);
+			syscall(334,buff);
+			finish_child++;
+			if(policy == 1)
+			{
+				int changeflag = 0;
+				int j = (now_run + 1)%child_num;
+				while(j != now_run)
+				{
+					if(child_q[j].left_t == 0||child_q[j].rd_t > counter){
+					j = (j + 1)%child_num;
+					continue;
+					}
+					now_run = j;
+					if(child_q[j].left_t == -1){
+						fprintf(stderr,"why");
+						child_q[j].left_t = child_q[j].ex_t;
+						setACTIVE(child_q[now_run].pid);
+						syscall(333, &child_q[j].start_sec,&child_q[j].start_nsec);
+					}
+					else
+						setACTIVE(child_q[now_run].pid);
+					fprintf(stderr,"dead change now child = %s\n",child_q[j].name);
+					t_q = 0;
+					changeflag = 1;
+					break;
+				}
+				if(!changeflag)
+					now_run = -1;
+			}
+			else
+				now_run = -1;
+		}
+
+
 		if(now_run == -1)
 		{
-			if(policy == 0)//FIFO
+			if(policy == 0||policy == 1)//FIFO+RR
 			{ 
 				for(int j = 0;j < child_num;j++)
 				{
@@ -135,17 +174,51 @@ int main(int argc, char* argv[])
 					child_q[j].left_t = child_q[j].ex_t;
 					setACTIVE(child_q[now_run].pid);
 					fprintf(stderr,"now child = %s\n",child_q[j].name);
+					syscall(333, &child_q[j].start_sec,&child_q[j].start_nsec);
+					t_q = 0;
 					break;
 				}
 			}
 			
 				
 		}
-		
+		else if(now_run > -1 && t_q == 501)
+		{
+			if(policy==1){
+				setIDLE(child_q[now_run].pid);
+				int j = (now_run + 1)%child_num;
+				int changeflag = 0;
+				while(j != now_run)
+				{
+					if(child_q[j].left_t == 0||child_q[j].rd_t > counter){
+						j = (j + 1)%child_num;
+						continue;
+					}
+					now_run = j;
+					if(child_q[j].left_t == -1){
+						child_q[j].left_t = child_q[j].ex_t;
+						setACTIVE(child_q[now_run].pid);
+						syscall(333, &child_q[j].start_sec,&child_q[j].start_nsec);
+					}
+					else
+						setACTIVE(child_q[now_run].pid);
+					fprintf(stderr,"change now child = %s\n",child_q[j].name);
+					t_q = 0;
+					changeflag = 1;
+					break;
+				}
+				if(!changeflag)
+				{
+					setACTIVE(child_q[now_run].pid);
+					t_q = 0;
+				}
+			}
+		}	
 		unittime();
 		if(now_run != -1)
 			child_q[now_run].left_t--;
 		counter++;
+		t_q++;
 	}
 	return 0;
 
